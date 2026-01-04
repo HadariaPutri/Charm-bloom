9b353d10a91c762384f10cc548449da54b7b1f25
Hadaria <hadaria22si@mahasiswa.pcr.ac.id>
2026-01-04 20:31:54 +0700

    perbaiki css

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['cart'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Main CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="{{asset(assets/css/style.css)}}">
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
